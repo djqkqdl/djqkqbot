b6fedeb2560b6c69489b699572f6ee1ae192c572
djqkqdl <60332014+djqkqdl@users.noreply.github.com>
2020-01-27 11:30:32 +0900

    Update 어밥봇.py

--- 어밥봇.py
+++ 어밥봇.py
@@ -4,6 +4,7 @@ import os
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print("login")
@@ -11,8 +12,7 @@ async def on_ready():
     print(client.user.id)
     print("-----------------")
     await client.change_presence(game=discord.Game(name='', type=1))
-
-    
+ 
 @client.event
 async def on_message(message):
     if message.content.startswith("어밥봇 안녕"):
