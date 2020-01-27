752eb52cba132ea5dc082c6191faa706931d044d
djqkqdl <60332014+djqkqdl@users.noreply.github.com>
2020-01-27 10:38:17 +0900

    Update 어밥봇.py

--- 어밥봇.py
+++ 어밥봇.py
@@ -1,4 +1,6 @@
 import discord
+import os
+
 
 client = discord.Client()
 
@@ -108,5 +110,5 @@ async def on_message(message):
         await message.channel.send(" > 어밥봇은 미육이선배라고 불러요!!")
 
         
-        
-client.run("NjcwODQyNjU0MzU0ODMzNDE4.Xi0Q9g.ZLdhqUpsB7br9eDAt5RhWjMdKok")
+access_token = os.environ["bot"]        
+client.run(access_token)
