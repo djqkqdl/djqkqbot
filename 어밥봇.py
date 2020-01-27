acd5909f76650c07a9148cae34cb4d1a734271dd
djqkqdl <60332014+djqkqdl@users.noreply.github.com>
2020-01-27 10:59:50 +0900

    Update 어밥봇.py

--- 어밥봇.py
+++ 어밥봇.py
@@ -9,14 +9,15 @@ async def on_ready():
     print("login")
     print(client.user.name)
     print(client.user.id)
-    print("ready")
+    print("-----------------")
     await client.change_presence(game=discord.Game(name='', type=1))
 
+    
 @client.event
 async def on_message(message):
     if message.content.startswith("어밥봇 안녕"):
         await message.channel.send(" > 안녕안녕안녕~~~~~~~")
 
-        
+  
 access_token = os.environ["BOT_TOKEN"]        
 client.run(access_token)
